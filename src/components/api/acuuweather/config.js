7dd8446776267668c76e1e1f8ca42d65aa7d38f4
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2019-08-23 03:23:42 +0300

    Https

--- src/components/api/acuuweather/config.js
+++ src/components/api/acuuweather/config.js
@@ -1,8 +1,8 @@
 
 export const apiKey = 'F6rJH4IEQrOzGcZUX4Qh7BZ1WmH1GFug';
 export const endPoints = {
-    geoposition: `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=`,
-    locationKey: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
-    autocompleteSearch: `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=`,
-    currentConditions: `http://dataservice.accuweather.com/currentconditions/v1/`,
+    geoposition: `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=`,
+    locationKey: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+    autocompleteSearch: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=`,
+    currentConditions: `https://dataservice.accuweather.com/currentconditions/v1/`,
 };
